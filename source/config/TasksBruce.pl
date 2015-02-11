39126d18e296184e38550c2758c27388b774c20c
Bruce Parrello <bruce@durabledd.com>
2015-02-11 09:58:22 -0600

    Added task list.

---
+++ source/config/TasksBruce.pl
@@ -0,0 +1,13 @@
+=head1 Bruce's Task List
+
+This file contains my to-do list. Everything comment that
+begins with TODO appears in the Eclipse Tasks pane on all
+my machines. Update this file to give me a task.
+
+=head2 Bruce Parrello
+
+=cut
+
+    ##TODO Create a test script that displays roles with EC numbers
+    ##TODO Create a script that loads tables from load files
+    ##TODO Fix the DocumentRoot problem
