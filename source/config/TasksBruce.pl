b312d95ccc85df3f3cec5adc40f7714a074b83a4
Bruce Parrello <bparrello@figresearch.com>
2015-02-12 06:55:35 -0600

    Updated task list. Changed some defaults.

--- source/config/TasksBruce.pl
+++ source/config/TasksBruce.pl
@@ -8,6 +8,4 @@ my machines. Update this file to give me a task.
 
 =cut
 
-    ##TODO Create a script that loads tables from load files
-    ##TODO Apply above script to Genome tables
     ##TODO Fix the DocumentRoot problem
